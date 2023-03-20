07b1ec5d1004dce0f51982144cc5bc8a40aec261
alexandros <alex.korolis@gmail.com>
2023-03-20 15:05:37 +0100

    modified html,css,javascript files

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-        let playerSelection = prompt("Choose rock ,  paper or scissors !!");
         let computerSelection = getComputerChoice();
         let playerWins = 0;
         let computerWins = 0;
@@ -54,14 +53,71 @@
             }
         }
 
-        function game()
-        {
-            for(i=0; i<5; i++)
+        const btnRock = document.querySelector('.btnRock');
+        const btnPaper = document.querySelector('.btnPaper');
+        const btnScissors = document.querySelector('.btnScissors');
+        let score = document.querySelector('.score');
+        let outPut = document.querySelector('.outPut');
+
+        btnRock.addEventListener('click',function rockPress() {
+            if(playerWins == 5)
             {
-                let result = playRound(playerSelection,computerSelection);
-                return result;
+                outPut.textContent = "Congratulations !! You Win !!";
+                btnRock.removeEventListener('click',rockPress);
             }
-        }
+            else if(computerWins == 5)
+            {
+                outPut.textContent = "Computer Wins";
+                btnRock.removeEventListener('click',rockPress);
+            }
+            else
+            {
+                computerSelection = getComputerChoice();
+                outPut.textContent = playRound('rock',computerSelection);
+                score.textContent = "You vs Computer : "+playerWins+" "+computerWins;
+            }
+        });
+
+        btnPaper.addEventListener('click',function paperPress() {
+            if(playerWins == 5)
+            {
+                outPut.textContent = "Congratulations !! You Win !!";
+                btnPaper.removeEventListener('click',paperPress);
+            }
+            else if(computerWins == 5)
+            {
+                outPut.textContent = "Computer Wins";
+                btnPaper.removeEventListener('click',paperPress);
+            }
+            else
+            {
+                computerSelection = getComputerChoice();
+                outPut.textContent = playRound('paper',computerSelection)
+                score.textContent = "You vs Computer : "+playerWins+" "+computerWins;
+            }
+        })
+
+        btnScissors.addEventListener('click',function scissorsPress() {
+            if(playerWins == 5)
+            {
+                outPut.textContent = "Congratulations !! You Win !!";
+                btnScissors.removeEventListener('click',scissorsPress);
+            }
+            else if(computerWins == 5)
+            {
+                outPut.textContent = "Computer Wins";
+                btnScissors.removeEventListener('click',scissorsPress);
+            }
+            else
+            {
+                computerSelection = getComputerChoice();
+                outPut.textContent = playRound('scissors',computerSelection)
+                score.textContent = "You vs Computer : "+playerWins+" "+computerWins;
+            }
+        })
+    
+
+
 
 
 
